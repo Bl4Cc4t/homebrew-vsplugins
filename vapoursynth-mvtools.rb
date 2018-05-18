20020f2a301d527cd4ab80779a15d4737f17f6fc
Kuro Neko <kuro@kn-mbp.fritz.box>
2018-05-18 19:36:44 +0200

    a

--- vapoursynth-mvtools.rb
+++ vapoursynth-mvtools.rb
@@ -16,7 +16,7 @@ class VapoursynthMvtools < Formula
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
-    #lib.install_symlink lib/"libmvtools.dylib" => "vapoursynth/libmvtools.dylib"
+    lib.install_symlink lib/"libmvtools.dylib" => "vapoursynth/libmvtools.dylib"
 
   end
 end
