161cf6c94cc1fc948290ba90fb692889c3e4c859
Kuro Neko <kuro@kn-mbp.fritz.box>
2018-05-18 20:47:44 +0200

    a

--- vapoursynth-mvtools.rb
+++ vapoursynth-mvtools.rb
@@ -1,8 +1,9 @@
 class VapoursynthMvtools < Formula
   desc "Motion compensation and stuff"
-  homepage ""
+  homepage "https://github.com/dubhater/vapoursynth-mvtools"
   url "https://github.com/dubhater/vapoursynth-mvtools/archive/v19.tar.gz"
   sha256 "41848bf526f1807e6894513534d5243bbce5b796d798a3cf47f617229d7b6e9e"
+  head "https://github.com/dubhater/vapoursynth-mvtools.git"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -16,8 +17,8 @@ class VapoursynthMvtools < Formula
     system "./autogen.sh"
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
-    ln_sf Dir[lib/"*.dylib"], (HOMEBREW_PREFIX/"lib/vapoursynth")
+    #ln_sf Dir[lib/"*.dylib"], (HOMEBREW_PREFIX/"lib/vapoursynth")
     #(HOMEBREW_PREFIX/"lib/vapoursynth").install_symlink Dir[lib/"*.dylib"]
-
+    system "ln", "-s", lib/"*.dylib", HOMEBREW_PREFIX/"lib/vapoursynth/"
   end
 end
