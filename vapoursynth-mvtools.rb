c90f54103e5acea18c43be74b20eebf9c9c7ec7a
Bl4Cc4t <Bl4Cc4t@users.noreply.github.com>
2018-05-18 18:14:30 +0200

    Update vapoursynth-mvtools.rb
    added deps

--- vapoursynth-mvtools.rb
+++ vapoursynth-mvtools.rb
@@ -4,6 +4,11 @@ class VapoursynthMvtools < Formula
   url "https://github.com/dubhater/vapoursynth-mvtools/archive/v19.tar.gz"
   sha256 "41848bf526f1807e6894513534d5243bbce5b796d798a3cf47f617229d7b6e9e"
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  
   def install
     system "./autogen.sh"
     system "./configure"
