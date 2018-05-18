299ac7b56d7834800cc7c9c5e90bcabba8e13bf5
Kuro Neko <kuro@kn-mbp.fritz.box>
2018-05-18 18:07:27 +0200

    test

---
+++ vapoursynth-mvtools.rb
@@ -0,0 +1,12 @@
+class VapoursynthMvtools < Formula
+  desc "Motion compensation and stuff"
+  homepage ""
+  url "https://github.com/dubhater/vapoursynth-mvtools/archive/v19.tar.gz"
+  sha256 "41848bf526f1807e6894513534d5243bbce5b796d798a3cf47f617229d7b6e9e"
+
+  def install
+    system "./autogen.sh"
+    system "./configure"
+    system "make", "install"
+  end
+end
