b7940df267551ece3f4bd4f3044ace7a6e656e06
Bl4Cc4t <kuron3ko@cock.li>
2018-05-20 00:17:39 +0200

    ayy

--- vsp-znedi3.rb
+++ vsp-znedi3.rb
@@ -2,7 +2,7 @@ class VspZnedi3 < Formula
   desc "nnedi3 filter"
   homepage "https://github.com/sekrit-twc/znedi3"
   version "r1"
-  url "https://github.com/sekrit-twc/znedi3/archive/r1.tar.gz"
+  #url "https://github.com/sekrit-twc/znedi3/archive/r1.tar.gz"
   sha256 "5a0a00f3783ececf3d430cdafea6bd7815a1a4c01ea2a8a19b01ae8a4e2d031d"
   head "https://github.com/sekrit-twc/znedi3.git"
 
