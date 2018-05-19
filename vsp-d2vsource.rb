6b4dac0c762c4a9ddea1fd2a45032cb0b386d1fb
Bl4Cc4t <kuron3ko@cock.li>
2018-05-19 23:41:30 +0200

    ayy

--- vsp-d2vsource.rb
+++ vsp-d2vsource.rb
@@ -2,6 +2,7 @@ class VspD2vsource < Formula
   desc "D2V parser and decoder for VapourSynth"
   homepage "https://github.com/dwbuiten/d2vsource"
   url "https://github.com/dwbuiten/d2vsource/archive/v1.1.tar.gz"
+  version "1.1"
   sha256 "e7c33a7bfde29c5f539a9c7e78c14c43b3c8248b861eee0845f6c77081b7fd77"
   head "https://github.com/dwbuiten/d2vsource.git"
 
@@ -9,7 +10,7 @@ class VspD2vsource < Formula
   depends_on "automake" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
-  depends_on "cmake" => :build
+  #depends_on "cmake" => :build
   depends_on "vapoursynth"
   depends_on "ffmpeg"
   #depends_on "fftw"
