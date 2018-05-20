c2ec568a358050f167dfe8f036af0c60c00ef08c
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 01:18:34 +0200

    blubb

--- Formula/vsp-sangnommod.rb
+++ Formula/vsp-sangnommod.rb
@@ -1,10 +1,10 @@
 class VspSangnommod < Formula
   desc "SangNom2 Plugin Ported for VapourSynth. Beat by James1201 on https://bitbucket.org/James1201/vapoursynth-sangnom/ "
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod"
-  version "0.1-fix"
-  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod/archive/v0.1-fix.tar.gz"
-  sha256 "ca918925a8bcbb2ad5318596db57110375da53128eb299e9b540a1d16e300000"
-  head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod.git"
+  # version "0.1-fix"
+  # url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-SangNomMod/archive/v0.1-fix.tar.gz"
+  # sha256 "ca918925a8bcbb2ad5318596db57110375da53128eb299e9b540a1d16e300000"
+  head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Bl4Cc4t.git"
 
   depends_on "vapoursynth"
   depends_on "zimg"
