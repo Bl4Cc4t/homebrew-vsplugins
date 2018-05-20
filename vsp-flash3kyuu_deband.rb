fbdb6effa19b36d66b8da0c0c49301a232913f8a
Bl4Cc4t <kuron3ko@cock.li>
2018-05-20 19:15:05 +0200

    new: f3kdb

---
+++ vsp-flash3kyuu_deband.rb
@@ -0,0 +1,22 @@
+class VspFlash3kyuuDeband < Formula
+  desc "A deband library and filter for avisynth/vapoursynth"
+  homepage "http://www.nmm-hd.org/newbbs/viewtopic.php?f=7&t=239"
+  version "2.0.0-1"
+  url "https://github.com/SAPikachu/flash3kyuu_deband/archive/2.0.0-1.tar.gz"
+  sha256 "5f68d017b45f8aecfeee3ac5343964c71eb8cbc209711aa5ad4dc9408f90f411"
+  head "https://github.com/FFMS/ffms2.git"
+
+  depends_on "cmake" => :build
+  depends_on "python" => :build
+
+  def install
+    system "python3", "waf", "configure"
+    system "python3", "waf", "build"
+    system "python3", "waf", "install"
+  end
+  def post_install
+    ohai ""
+    ohai "When you’re done installing plugins:"
+    ohai "Please run linkvsp.sh to link the libraries."
+  end
+end
