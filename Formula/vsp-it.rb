fb3e32ae77a8472c2e767960ce8c0900a024aa87
Bl4Cc4t <kuron3ko@cock.li>
2018-05-20 23:41:56 +0200

    blubb

---
+++ Formula/vsp-it.rb
@@ -0,0 +1,24 @@
+class VspIt < Formula
+  desc "Inverse Telecine Plugin Ported for VapourSynth"
+  homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT"
+  version "1.2"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT/archive/v1.2.tar.gz"
+  sha256 "4f354c1d215e6b30087d68a116e73f6fce24eb99d4fe788b6dc1fb9f4b00cfff"
+  head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT.git"
+
+  # depends_on "libtool" => :build
+  # depends_on "pkg-config" => :build
+  # depends_on "vapoursynth"
+
+  def install
+    system "./configure"
+    system "make"
+    prefix.install Dir["*.md"]
+    lib.install Dir["*.dylib"]
+  end
+  def post_install
+    ohai ""
+    ohai "When you’re done installing plugins:"
+    ohai "Please run linkvsp.sh to link the libraries."
+  end
+end
