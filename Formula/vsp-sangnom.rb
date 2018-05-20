9faa2e28580158f6738a76b0e914b49bf738ea77
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 01:41:37 +0200

    new: sangnom

--- Formula/vsp-sangnom.rb
+++ Formula/vsp-sangnom.rb
@@ -4,7 +4,7 @@ class VspSangnom < Formula
   # version "r40"
   # url "https://bitbucket.org/James1201/vapoursynth-sangnom/get/default.tar.gz"
   # sha256 "b78ba8edd6a7d856c0606de822111095e64b1f4d8cbbc311a5c52cca62d6137b"
-  head "https://bitbucket.org/James1201/vapoursynth-sangnom"
+  head "https://bitbucket.org/James1201/vapoursynth-sangnom", :using => :hg
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
