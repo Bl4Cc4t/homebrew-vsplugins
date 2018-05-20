2fcc8f195e70245ab5d560fef1d5fbac286d4add
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 01:07:32 +0200

    blubb

--- Formula/vsp-it.rb
+++ Formula/vsp-it.rb
@@ -1,20 +1,18 @@
 class VspIt < Formula
   desc "Inverse Telecine Plugin Ported for VapourSynth"
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT"
-  version "1.2"
-  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT/archive/v1.2.tar.gz"
-  sha256 "4f354c1d215e6b30087d68a116e73f6fce24eb99d4fe788b6dc1fb9f4b00cfff"
-  head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT.git"
+  # version "1.2"
+  # url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-IT/archive/v1.2.tar.gz"
+  # sha256 "4f354c1d215e6b30087d68a116e73f6fce24eb99d4fe788b6dc1fb9f4b00cfff"
+  head "https://github.com/Bl4Cc4t/VapourSynth-IT.git"
 
   depends_on "pkg-config" => :build
   depends_on "vapoursynth"
   depends_on "zimg"
 
   def install
-    system "./configure"
-    system "make"
-    prefix.install Dir["*.md"]
-    lib.install Dir["*.dylib"]
+    system "./configure", "--install=#{lib}"
+    system "make", "install"
   end
   def post_install
     ohai ""
