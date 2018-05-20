d6535e19670fbe89d300ac1eb48391c89881d727
Bl4Cc4t <kuron3ko@cock.li>
2018-05-20 20:02:08 +0200

    ayy

--- vsp-fmtconv.rb
+++ vsp-fmtconv.rb
@@ -12,8 +12,7 @@ class VspFmtconv < Formula
   depends_on "libtool" => :build
 
   def install
-    system "cd", "build/unix"
-    system "ls"
+    Dir.chdir("build/unix")
     system "./autogen.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
