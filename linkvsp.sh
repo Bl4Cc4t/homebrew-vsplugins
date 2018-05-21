f921a10233070a687729111d077bba5f41feb245
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 02:00:34 +0200

    changed linkvsp

--- linkvsp.sh
+++ linkvsp.sh
@@ -4,13 +4,15 @@
 
 ctr=0
 for d in $(find /usr/local/Cellar -type d -name "vsp-*"); do
-  echo "Found $(basename $d)!"
+  #echo "Found $(basename $d)!"
   for f in $d/*/lib/*; do
-    fn=$(basename $f)
-    if [ ! -e /usr/local/lib/vapoursynth/$fn ]; then
-      echo "Missing symlink: $fn"
-      ln -s $f /usr/local/lib/vapoursynth/$fn
-      ctr=$((ctr+1))
+    if [[ ! -L $f && -f $f ]]; then
+      fn=$(basename $f)
+      if [ ! -e /usr/local/lib/vapoursynth/$fn ]; then
+        echo "Found missing symlink: $fn"
+        ln -s $f /usr/local/lib/vapoursynth/$fn
+        ctr=$((ctr+1))
+      fi
     fi
   done
 done
