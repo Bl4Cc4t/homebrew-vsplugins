3ad0c572109e40739bab3e52478296a99a3dd440
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 17:48:22 +0200

    added stuff

--- Formula/vsp-vfrtocfr.rb
+++ Formula/vsp-vfrtocfr.rb
@@ -16,7 +16,7 @@ class VspVfrtocfr < Formula
     system "mkdir", "#{buildpath}/build"
     #Dir.chdir("#{buildpath}/build")
     system "cd", "#{buildpath}/build"
-    system "mesonconf", "-Dcpp_std=c++11"
+    system "meson configure", "-Dcpp_std=c++11"
     system "meson", "--prefix #{prefix} --buildtype release .."
     system "ninja"
     system "ninja", "install"
