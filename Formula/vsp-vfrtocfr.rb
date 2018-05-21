696e7d762ae46bacdd9aa752abbffecfaf166d79
Bl4Cc4t <kuron3ko@cock.li>
2018-05-21 17:26:13 +0200

    added stuff

--- Formula/vsp-vfrtocfr.rb
+++ Formula/vsp-vfrtocfr.rb
@@ -1,9 +1,9 @@
 class VspVfrtocfr < Formula
   desc "Vapoursynth port of VFRToCFR"
   homepage "https://github.com/Irrational-Encoding-Wizardry/Vapoursynth-VFRToCFR"
-  version "r2"
-  url "https://github.com/Irrational-Encoding-Wizardry/Vapoursynth-VFRToCFR/archive/v1.0.tar.gz"
-  sha256 "d0bfc415c9bcca0711a19f72f249280de53c93440e1a61bf907d6ebe5a7bfe85"
+  # version "r2"
+  # url "https://github.com/Irrational-Encoding-Wizardry/Vapoursynth-VFRToCFR/archive/v1.0.tar.gz"
+  # sha256 "d0bfc415c9bcca0711a19f72f249280de53c93440e1a61bf907d6ebe5a7bfe85"
   head "https://github.com/Irrational-Encoding-Wizardry/Vapoursynth-VFRToCFR.git"
 
   depends_on "meson" => :build
@@ -12,8 +12,7 @@ class VspVfrtocfr < Formula
   def install
     (buildpath"/build").mkpath
     Dir.chdir("#{buildpath}/build")
-    system "meson", "--prefix #{prefix}",
-                    "--buildtype release .."
+    system "meson", "--prefix #{prefix} --buildtype release .."
     system "ninja"
     system "ninja", "install"
   end
