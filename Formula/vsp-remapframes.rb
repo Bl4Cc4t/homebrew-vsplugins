1f1bf75e9adb616c76430f83eb290296cb6d618a
Bl4Cc4t <kuron3ko@cock.li>
2018-07-04 16:47:32 +0200

    added remapframes

--- Formula/vsp-remapframes.rb
+++ Formula/vsp-remapframes.rb
@@ -11,10 +11,9 @@ class VspRemapframes < Formula
   depends_on "vapoursynth"
 
   def install
-    # system "meson", "configure", "-Dcpp_std=c++11"
     (prefix/"build").mkpath
-    system "meson", ".", "build"
     Dir.chdir("#{prefix}/build")
+    system "meson", "configure", "-Dcpp_std=c++11"
     system "meson", "--prefix #{prefix}",
                     "--buildtype release .."
     system "ninja"
