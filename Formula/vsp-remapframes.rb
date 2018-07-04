2666d516f84302fb70b28c6c119c8b4f44bfe541
Bl4Cc4t <kuron3ko@cock.li>
2018-07-04 17:01:31 +0200

    added remapframes

--- Formula/vsp-remapframes.rb
+++ Formula/vsp-remapframes.rb
@@ -15,6 +15,7 @@ class VspRemapframes < Formula
     # system "meson", "configure", "-Dcpp_std=c++11"
     # (buildpath/"build").mkpath
     # Dir.chdir("#{buildpath}/build")
+    system "echo $PWD"
     system "meson", "--prefix #{prefix}",
                     "--buildtype release .."
     system "ninja"
