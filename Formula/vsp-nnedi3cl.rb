b9c08fc9bbec357c6074e45da4eeed6121790cce
Bl4Cc4t <kuron3ko@cock.li>
2018-12-30 19:31:36 +0100

    NNEDI3CL r7.2

--- Formula/vsp-nnedi3cl.rb
+++ Formula/vsp-nnedi3cl.rb
@@ -1,9 +1,9 @@
 class VspNnedi3cl < Formula
   desc "VapourSynth port of NNEDI3 utilizing OpenCL"
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL"
-  version "r7"
-  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL/archive/r7.tar.gz"
-  sha256 "11890b2ca27c5127068579ca3c72c191dca0563483e4c565ec83cdacee4bfb51"
+  version "r7.2"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL/archive/r7.2.tar.gz"
+  sha256 "b088c9828d2145164f3d36f0c59a01fe90a004ec46c2ef8c51f0d48eac4ee344"
   head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-NNEDI3CL.git"
 
   depends_on "autoconf" => :build
@@ -12,7 +12,7 @@ class VspNnedi3cl < Formula
   depends_on "libtool" => :build
   depends_on "boost" => :build
   depends_on "vapoursynth"
-  
+
   def install
     system "./autogen.sh"
     system "./configure", "--disable-debug",
