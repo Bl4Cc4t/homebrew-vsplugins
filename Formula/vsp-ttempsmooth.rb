abbad3960fe566fda7e0a8c0fc3028d44b10feb5
Bl4Cc4t <kuron3ko@cock.li>
2018-12-30 20:01:52 +0100

    TTempSmooth r3.1

--- Formula/vsp-ttempsmooth.rb
+++ Formula/vsp-ttempsmooth.rb
@@ -1,23 +1,24 @@
 class VspTtempsmooth < Formula
   desc "VapourSynth port of TTempSmooth"
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TTempSmooth"
-  version "r3"
-  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TTempSmooth/archive/r3.tar.gz"
-  sha256 "e7434f2deb9a465c77d3ce3f17661b02ce4a48e466be0d10809d6f741caf3cfb"
+  version "r3.1"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TTempSmooth/archive/r3.1.tar.gz"
+  sha256 "c68db30aad8e596969bf47c7c95c1375ec86659f377ff0e3adb5f566b83f5a59"
   head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TTempSmooth.git"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "libtool" => :build
+  depends_on "boost" => :build
   depends_on "vapoursynth"
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    inreplace "meson.build",
+              "install_dir : join_paths(vapoursynth_dep.get_pkgconfig_variable('libdir'), 'vapoursynth')",
+              "install_dir : '#{lib}'"
+    system "meson", "build"
+    system "ninja", "-C", "build"
+    system "ninja", "-C", "build", "install"
   end
   def post_install
     ohai ""
