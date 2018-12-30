9fceb86df3b828e6e8cb620d293c121a6a655f37
Bl4Cc4t <kuron3ko@cock.li>
2018-12-30 19:37:51 +0100

    Retinex r4

--- Formula/vsp-retinex.rb
+++ Formula/vsp-retinex.rb
@@ -1,18 +1,25 @@
 class VspRetinex < Formula
   desc "Retinex algorithm for VapourSynth"
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Retinex"
-  # version "r3"
-  # url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Retinex/archive/r3.tar.gz"
-  # sha256 "fe5e65803f43452097f8315a1b800d5e137e5ef6fb5b326f177693d5a77ac1d4"
+  version "r4"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Retinex/archive/r4.tar.gz"
+  sha256 "979b1f8d0dc04a5501a4393873bff538ead5d2c822a0a29a10de02c146e77e8c"
   head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-Retinex.git"
 
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
+  depends_on "boost" => :build
   depends_on "vapoursynth"
   depends_on "zimg"
 
   def install
-    system "./configure", "--install=#{lib}"
-    system "make", "install"
+    inreplace "meson.build",
+              "install_dir : join_paths(vapoursynth_dep.get_pkgconfig_variable('libdir'), 'vapoursynth')",
+              "install_dir : '#{lib}'"
+    system "meson", "build"
+    system "ninja", "-C", "build"
+    system "ninja", "-C", "build", "install"
   end
   def post_install
     ohai ""
