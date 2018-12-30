b967d270509d5e3e22eb5450d08269b2c70fb404
Bl4Cc4t <kuron3ko@cock.li>
2018-12-30 19:53:51 +0100

    TDeintMod r10.1

--- Formula/vsp-tdeintmod.rb
+++ Formula/vsp-tdeintmod.rb
@@ -1,23 +1,24 @@
 class VspTdeintmod < Formula
   desc "VapourSynth port of TDeint + TMM"
   homepage "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TDeintMod"
-  # version "r10"
-  # url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TDeintMod/archive/r10.tar.gz"
-  # sha256 "46801e5620e7b012de6c040d60225a6fa8ec63afe78218d7f0edc360962a085d"
+  version "r10.1"
+  url "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TDeintMod/archive/r10.1.tar.gz"
+  sha256 "0d17d9998251c13e79412f70bc7b28480be6fb419cf31cee24d4c35be0a1ac85"
   head "https://github.com/HomeOfVapourSynthEvolution/VapourSynth-TDeintMod.git"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "libtool" => :build
+  depends_on "boost" => :build
   depends_on "vapoursynth"
 
   def install
-    system "./autogen.sh"
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
-    system "make", "install"
+    inreplace "meson.build",
+              "install_dir : join_paths(vapoursynth_dep.get_pkgconfig_variable('libdir'), 'vapoursynth')",
+              "install_dir : '#{lib}'"
+    system "meson", "build"
+    system "ninja", "-C", "build"
+    system "ninja", "-C", "build", "install"
   end
   def post_install
     ohai ""
